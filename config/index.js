f098da49a370f4130c6660bad29442cd837c4e6d
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-14 20:00:24 -0600

    cambie config!

--- config/index.js
+++ config/index.js
@@ -2,13 +2,13 @@ const NODE_ENV = process.env.NODE_ENV || 'STAGING';
 
 const config = {
   PRODUCTION: {
-    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cintanegrab42-vdmib.mongodb.net/test?authSource=admin&replicaSet=CintaNegraB42-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vbq4z.mongodb.net/guali?retryWrites=true&w=majority`,
   },
   STAGING: {
-    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cintanegrab42-vdmib.mongodb.net/test?authSource=admin&replicaSet=CintaNegraB42-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vbq4z.mongodb.net/guali?retryWrites=true&w=majority`,
   },
   TEST: {
-    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cintanegrab42-vdmib.mongodb.net/test?authSource=admin&replicaSet=CintaNegraB42-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
+    MONGO_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.vbq4z.mongodb.net/guali?retryWrites=true&w=majority`,
   },
 };
 // eslint-disable-next-line no-console
