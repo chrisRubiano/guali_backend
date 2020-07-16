ddf361448d31227a60586b1043b56d3dd963eceb
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-15 19:58:04 -0600

    Cree Movements y lo pase al index

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable indent */
 const User = require('./User');
 const { Post } = require('./Post');
+const Movements = require('./Movements');
 
 module.exports = {
     User,
     Post,
+    Movements,
 };
