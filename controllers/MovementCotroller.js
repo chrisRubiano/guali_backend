125c4fa7ffcd6f1a5b94b940a4bdcbc1e46b1f0c
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-15 20:33:56 -0600

    cambie MovementService

--- controllers/MovementCotroller.js
+++ controllers/MovementCotroller.js
@@ -1,4 +1,4 @@
-const { UserService, PostService } = require('../services');
+const { UserService, MovementService } = require('../services');
 
 module.exports = {
   create: async (req, res) => {
@@ -6,7 +6,7 @@ module.exports = {
     const { body } = req;
     try {
       const user = await UserService.findOneById(idUser);
-      const post = await PostService.create(body);
+      const post = await MovementService.create(body);
       const userWithPost = await UserService.addPost(user, post);
       res.status(201).json(userWithPost);
     } catch (error) {
@@ -27,7 +27,7 @@ module.exports = {
     const { idUser, idPost } = req.params;
     try {
       const user = await UserService.findOneById(idUser);
-      const post = PostService.findOneByIdInUser(idPost, user);
+      const post = MovementService.findOneByIdInUser(idPost, user);
       if (!post) res.status(404).json({ message: 'Post not found' });
       res.status(200).json(post);
     } catch (error) {
@@ -39,9 +39,9 @@ module.exports = {
     try {
       const user = await UserService.findOneById(idUser);
       if (!user) res.status(404).json({ message: 'User not found' });
-      const post = PostService.findOneByIdInUser(idPost, user);
+      const post = MovementService.findOneByIdInUser(idPost, user);
       if (!post) res.status(404).json({ message: 'Post not found' });
-      await PostService.updateOneByIdInUser(idPost, user, { is_active: false });
+      await MovementService.updateOneByIdInUser(idPost, user, { is_active: false });
       res.status(204).json();
     } catch (error) {
       res.status(400).json({ message: 'Error getting user post by id', error });
