e8acc75a00f338a26502227bebeaa3453f3efd5a
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-15 20:10:39 -0600

    cambie nombre

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable indent */
 const User = require('./User');
 const { Post } = require('./Post');
-const Movements = require('./Movements');
+const Movements = require('./Movement');
 
 module.exports = {
     User,
