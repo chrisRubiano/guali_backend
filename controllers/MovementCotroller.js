2a3af3cdc1f8f44e20dba2562b6a69cd94af403f
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-07-15 20:19:19 -0600

    quite comentarios

--- controllers/MovementCotroller.js
+++ controllers/MovementCotroller.js
@@ -5,13 +5,9 @@ module.exports = {
     const { idUser } = req.params;
     const { body } = req;
     try {
-      // 1) Traemos al usuario
       const user = await UserService.findOneById(idUser);
-      // 2) Crear el objeto de posts
       const post = await PostService.create(body);
-      // 3) Agregar el nuevo post al usuario y salvar el usuario
       const userWithPost = await UserService.addPost(user, post);
-      // 4) Responder al cliente con el usuario con el nuevo post
       res.status(201).json(userWithPost);
     } catch (error) {
       res.status(400).json({ message: 'error adding post to user', error });
@@ -20,13 +16,8 @@ module.exports = {
   findAll: async (req, res) => {
     const { idUser } = req.params;
     try {
-      // 1) Traemos el usuario
       const user = await UserService.findOneById(idUser);
-
-      // 2) Sacamos los posts del objeto de usuario
       const { posts } = user;
-
-      // 3) Responder al cliente con los posts del usuario
       res.status(200).json(posts);
     } catch (error) {
       res.status(400).json({ message: 'Error getting user posts', error });
@@ -35,14 +26,9 @@ module.exports = {
   findOne: async (req, res) => {
     const { idUser, idPost } = req.params;
     try {
-      // 1) Traemos el usuario
       const user = await UserService.findOneById(idUser);
-
-      // 2) Sacamos el post deseado del objeto de usuario
       const post = PostService.findOneByIdInUser(idPost, user);
       if (!post) res.status(404).json({ message: 'Post not found' });
-
-      // 3) Responder al cliente con el post deseado del usuario
       res.status(200).json(post);
     } catch (error) {
       res.status(400).json({ message: 'Error getting user post by id', error });
@@ -51,18 +37,11 @@ module.exports = {
   deleteOne: async (req, res) => {
     const { idUser, idPost } = req.params;
     try {
-      // 1) Traemos el usuario
       const user = await UserService.findOneById(idUser);
       if (!user) res.status(404).json({ message: 'User not found' });
-
-      // 2) Sacamos el post deseado del objeto de usuario
       const post = PostService.findOneByIdInUser(idPost, user);
       if (!post) res.status(404).json({ message: 'Post not found' });
-
-      // 3) Actualizamos el post del usuario
       await PostService.updateOneByIdInUser(idPost, user, { is_active: false });
-
-      // 4) Responder al cliente con el post modificado
       res.status(204).json();
     } catch (error) {
       res.status(400).json({ message: 'Error getting user post by id', error });
